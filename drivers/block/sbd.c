c2864b529e8c013ff781fd54050df6c2ecba050b
Soo-Min Yoo <soomin.yoo95@gmail.com>
2017-05-22 12:29:11 -0700

    Add global variables and initialization for encryption

--- drivers/block/sbd.c
+++ drivers/block/sbd.c
@@ -1,4 +1,8 @@
 /*
+ *  Resources from:
+ *  https://lwn.net/Articles/58719/
+ *  http://blog.superpat.com/2010/05/04/a-simple-block-driver-for-linux-kernel-2-6-31/
+ *
  *  * A sample, extra-simple block driver. Updated for kernel 2.6.31.
  *   *
  *    * (C) 2003 Eklektix, Inc.
@@ -29,6 +33,13 @@ module_param(logical_block_size, int, 0);
 static int nsectors = 1024; /* How big the drive is */
 module_param(nsectors, int, 0);
 
+static char *key = "cryptokey";
+static int key_length = 10;
+struct crypto_cipher *cipher; //Crypto cipher struct. Critical for crypto.
+
+#define CIPHER_TYPE "aes" //Cipher algorithm to use
+
+
 /*
  *  * We can tweak our hardware sector size, but the kernel talks to us
  *   * in terms of small sectors, always.
@@ -55,6 +66,10 @@ static struct sbd_device {
  *   */
 static void sbd_transfer(struct sbd_device *dev, sector_t sector,
 		unsigned long nsect, char *buffer, int write) {
+	
+	/* add encryption code */
+
+
 	unsigned long offset = sector * logical_block_size;
 	unsigned long nbytes = nsect * logical_block_size;
 
@@ -112,6 +127,8 @@ static struct block_device_operations sbd_ops = {
 };
 
 static int __init sbd_init(void) {
+	
+	crypt = crypto_alloc_cipher(CIPHER_TYPE, 0, 0);
 	/*
 	 *	 * Set up our internal device.
 	 *		 */
@@ -161,6 +178,8 @@ out:
 
 static void __exit sbd_exit(void)
 {
+	crypto_free_cipher(crypt);
+
 	del_gendisk(Device.gd);
 	put_disk(Device.gd);
 	unregister_blkdev(major_num, "sbd");
