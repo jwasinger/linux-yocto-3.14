66dfa6cd1c50a7e5ba6c36a4452b03a619685223
j-wasinger@hotmail.com <j-wasinger@hotmail.com>
2017-05-22 23:44:59 -0700

    final commit a3

--- drivers/block/sbd.c
+++ drivers/block/sbd.c
@@ -1,14 +1,14 @@
 /*
- *  Resources from:
- *  https://lwn.net/Articles/58719/
- *  http://blog.superpat.com/2010/05/04/a-simple-block-driver-for-linux-kernel-2-6-31/
+ * Based on SBD from:
+ * http://blog.superpat.com/2010/05/04/a-simple-block-driver-for-linux-kernel-2-6-31/
  *
- *  * A sample, extra-simple block driver. Updated for kernel 2.6.31.
- *   *
- *    * (C) 2003 Eklektix, Inc.
- *     * (C) 2010 Pat Patterson <pat at superpat dot com>
- *      * Redistributable under the terms of the GNU GPL.
- *       */
+ *
+ * A sample, extra-simple block driver. Updated for kernel 2.6.31.
+ *
+ * (C) 2003 Eklektix, Inc.
+ * (C) 2010 Pat Patterson <pat at superpat dot com>
+ * Redistributable under the terms of the GNU GPL.
+ */
 
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -22,6 +22,7 @@
 #include <linux/genhd.h>
 #include <linux/blkdev.h>
 #include <linux/hdreg.h>
+#include <linux/crypto.h> /* ADDED */
 
 MODULE_LICENSE("Dual BSD/GPL");
 static char *Version = "1.4";
@@ -33,64 +34,71 @@ module_param(logical_block_size, int, 0);
 static int nsectors = 1024; /* How big the drive is */
 module_param(nsectors, int, 0);
 
-static char *key = "cryptokey";
-static int key_length = 10;
-struct crypto_cipher *crypt; //Crypto cipher struct. Critical for crypto.
-
-#define CIPHER_TYPE "aes" //Cipher algorithm to use
+static char *key = "asdfasdfasdfasdfasdfasdfasdfsdfa";
+/* ADDED: Get real key from module param */
+module_param(key, charp, 0400);
 
 
 /*
- *  * We can tweak our hardware sector size, but the kernel talks to us
- *   * in terms of small sectors, always.
- *    */
+ * We can tweak our hardware sector size, but the kernel talks to us
+ * in terms of small sectors, always.
+ */
 #define KERNEL_SECTOR_SIZE 512
 
+/* ADDED: We'll be passing this value to several functions so defining it */
+#define KEY_SIZE 32
+
 /*
- *  * Our request queue.
- *   */
+ * Our request queue.
+ */
 static struct request_queue *Queue;
 
 /*
- *  * The internal representation of our device.
- *   */
+ * The internal representation of our device.
+ */
 static struct sbd_device {
 	unsigned long size;
 	spinlock_t lock;
 	u8 *data;
 	struct gendisk *gd;
+
+	/* ADDED: The following is added to sbd for encryption */
+	struct crypto_cipher *blockcipher;
+//	struct scatterlist sl[2];
 } Device;
 
 /*
- *  * Handle an I/O request.
- *   */
+ * Handle an I/O request.
+ */
 static void sbd_transfer(struct sbd_device *dev, sector_t sector,
 		unsigned long nsect, char *buffer, int write) {
-	
-	// set encryption key
-	crypto_cipher_setkey(crypt, key, key_length);
-	
-	// add encryption code
-	
-
-	unsigned long offset = sector * logical_block_size;
+	int i;
 	unsigned long nbytes = nsect * logical_block_size;
+	unsigned long offset = sector * logical_block_size;
+
 
 	if ((offset + nbytes) > dev->size) {
-		printk (KERN_NOTICE "sbd: Beyond-end write (%ld %ld)\n", offset, nbytes);
+		printk (KERN_NOTICE "offset beyond end of disk\n", offset, nbytes);
 		return;
 	}
-	if (write)
-		memcpy(dev->data + offset, buffer, nbytes);
-	else
-		memcpy(buffer, dev->data + offset, nbytes);
+
+	if (write) {
+		for (i = 0; i < nbytes; i+= crypto_cipher_blocksize(dev->blockcipher)) {
+			crypto_cipher_encrypt_one(dev->blockcipher, dev->data + offset + i, &buffer[i]);
+		}
+	} else {
+		for (i = 0; i < nbytes; i+= crypto_cipher_blocksize(dev->blockcipher)) {
+			crypto_cipher_decrypt_one(dev->blockcipher, &buffer[i], dev->data + offset + i);
+		}
+	}
 }
 
 static void sbd_request(struct request_queue *q) {
 	struct request *req;
 
 	req = blk_fetch_request(q);
-	while (req != NULL) {
+	while (req != NULL) 
+	{
 		if (req == NULL || (req->cmd_type != REQ_TYPE_FS)) {
 			printk (KERN_NOTICE "Skip non-CMD request\n");
 			__blk_end_request_all(req, -EIO);
@@ -104,15 +112,9 @@ static void sbd_request(struct request_queue *q) {
 	}
 }
 
-/*
- *  * The HDIO_GETGEO ioctl is handled in blkdev_ioctl(), which
- *   * calls this. We need to implement getgeo, since we can't
- *    * use tools such as fdisk to partition the drive otherwise.
- *     */
 int sbd_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
 	long size;
 
-	/* We have no real geometry, of course, so make something up. */
 	size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
 	geo->cylinders = (size & ~0x3f) >> 6;
 	geo->heads = 4;
@@ -121,46 +123,50 @@ int sbd_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
 	return 0;
 }
 
-/*
- *  * The device operations structure.
- *   */
 static struct block_device_operations sbd_ops = {
 	.owner  = THIS_MODULE,
 	.getgeo = sbd_getgeo
 };
 
 static int __init sbd_init(void) {
-	
-	crypt = crypto_alloc_cipher(CIPHER_TYPE, 0, 0);
-	/*
-	 *	 * Set up our internal device.
-	 *		 */
+	int err;
+
 	Device.size = nsectors * logical_block_size;
 	spin_lock_init(&Device.lock);
 	Device.data = vmalloc(Device.size);
+
 	if (Device.data == NULL)
 		return -ENOMEM;
-	/*
-	 *	 * Get a request queue.
-	 *		 */
+
 	Queue = blk_init_queue(sbd_request, &Device.lock);
 	if (Queue == NULL)
 		goto out;
+
 	blk_queue_logical_block_size(Queue, logical_block_size);
-	/*
-	 *	 * Get registered.
-	 *		 */
+
+	Device.blockcipher = crypto_alloc_cipher(
+		"aes", 0, CRYPTO_ALG_ASYNC);
+	if (IS_ERR(Device.blockcipher)) {
+        printk("block cipher invalid\n");
+        goto out;
+	}
+
+	err = crypto_cipher_setkey(Device.blockcipher, key, KEY_SIZE);
+	if (err != 0) {
+        printk("key not set");
+        goto out;
+	}
+
 	major_num = register_blkdev(major_num, "sbd");
 	if (major_num < 0) {
-		printk(KERN_WARNING "sbd: unable to get major number\n");
+		printk(KERN_WARNING "major number invalid\n");
 		goto out;
 	}
-	/*
-	 *	 * And the gendisk structure.
-	 *		 */
+
 	Device.gd = alloc_disk(16);
 	if (!Device.gd)
 		goto out_unregister;
+
 	Device.gd->major = major_num;
 	Device.gd->first_minor = 0;
 	Device.gd->fops = &sbd_ops;
@@ -181,8 +187,6 @@ out:
 
 static void __exit sbd_exit(void)
 {
-	crypto_free_cipher(crypt);
-
 	del_gendisk(Device.gd);
 	put_disk(Device.gd);
 	unregister_blkdev(major_num, "sbd");
